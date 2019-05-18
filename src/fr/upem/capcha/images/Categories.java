3d9d9ea4c398317b17273edd3ccad77882612acf
aubane marquer <aubane.marquer@gmail.com>
2019-05-18 17:52:09 +0200

    architecture beginning

---
+++ src/fr/upem/capcha/images/Categories.java
@@ -0,0 +1,32 @@
+package fr.upem.capcha.images;
+
+import java.net.URL;
+import java.util.List;
+
+public class Categories implements Images{
+
+	@Override
+	public List<URL> getPhotos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<URL> getRandomPhotosURL(int value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<URL> getRandomPhotoURL() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isPhotoCorrect(URL url) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/fr/upem/capcha/images/Images.java
@@ -0,0 +1,16 @@
+package fr.upem.capcha.images;
+
+import java.net.URL;
+import java.util.List;
+
+public interface Images {
+
+	public List<URL> getPhotos();
+	
+	public List<URL> getRandomPhotosURL(int value);
+
+	public List<URL> getRandomPhotoURL();
+	
+	public boolean isPhotoCorrect(URL url);
+
+}
