31023b13faf0d449f0a4f32718f00c2b8955006d
MarcoKouyate <marco.kouyate@yahoo.fr>
2019-05-20 23:47:48 +0200

    Added bus and bycicle

---
+++ src/fr/upem/capcha/images/vehicles/bikes/bycicles/Bycicle.java
@@ -0,0 +1,16 @@
+package fr.upem.capcha.images.vehicles.bikes.bycicles;
+import fr.upem.capcha.images.vehicles.bikes.Bike;
+
+public class Bycicle extends Bike {
+
+	public Bycicle() {
+		super();
+	}
+	
+	@Override
+	public String toString() {
+		return "byciclettes";
+	}
+
+}
+
---
+++ src/fr/upem/capcha/images/vehicles/four/bus/Bus.java
@@ -0,0 +1,15 @@
+package fr.upem.capcha.images.vehicles.four.bus;
+import fr.upem.capcha.images.vehicles.four.FourWheels;;
+
+public class Bus extends FourWheels {
+
+	public Bus() {
+		super();
+	}
+	
+	@Override
+	public String toString() {
+		return "bus";
+	}
+
+}
